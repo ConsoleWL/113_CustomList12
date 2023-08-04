f475a5ed1e8779d05845be758d1c15d238c5e835
Nikita Obergan <Obergannikita@gmail.com>
2023-08-04 00:41:53 -0400

    Add Add TestMethods

--- CustomListTests/AddTests.cs
+++ CustomListTests/AddTests.cs
@@ -1,11 +1,85 @@
+using CustomList;
+
 namespace CustomListTests
 {
     [TestClass]
     public class AddTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Add_CountIncrementsAsItemAdded_CountEqualsOne()
         {
+            //Arrange
+            CustomList<string> names = new CustomList<string>();
+
+            //Set
+            names.Add("Bob");
+
+            //Assert
+            Assert.AreEqual(1, names.Count);
+        }
+
+        [TestMethod]
+        public void Add_FirstItemAddedAtIndexZero_IndexZeroEqualsBob()
+        {
+            //Arrange
+            CustomList<string> names = new CustomList<string>();
+            //Set
+            names.Add("Bob");
+
+            //Assert
+            Assert.AreEqual("Bob", names[0]);
+        }
+
+        [TestMethod]
+        public void Add_SecondItemAddedAtIndexOne_IndexOneEqualsTom()
+        {
+            //Arrange
+            CustomList<string> names = new CustomList<string>();
+            //Set
+            names.Add("Bob");
+            names.Add("Tom");
+
+            //Assert
+            Assert.AreEqual("Tom", names[1]);
+        }
+
+        [TestMethod]
+        public void Add_CapacityIncreasesWhenExceeded_CapacityEqualsEight()
+        {
+            //Arrange
+            CustomList<string> names = new CustomList<string>();
+            int capasityBeforeIncreasing = names.Capacity;
+            int capasityAfterIncreasing;
+
+            //Set
+            names.Add("Bob");
+            names.Add("Bob");
+            names.Add("Bob");
+            names.Add("Bob");
+            names.Add("Bob");
+
+            capasityAfterIncreasing = names.Capacity;
+
+            //Assert
+            Assert.AreEqual(8, names.Capacity);
+        }
+
+        [TestMethod]
+        public void Add_WhenCapacityIncreasesOriginalItemsStayAtTheSamePosition_Index3EqualsBob()
+        {
+            //Arrange
+            CustomList<string> names = new CustomList<string>();
+
+            //Set
+            names.Add("Tom");
+            names.Add("Tos");
+            names.Add("Tod");
+            names.Add("Bob");
+            names.Add("Tog");
+            names.Add("Tot");
+            //Assert
+            Assert.AreEqual("Bob", names[3]);
+
         }
     }
 }
